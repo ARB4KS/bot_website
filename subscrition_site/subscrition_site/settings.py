45f51e1ec1526467925c5c1bfb3d2f3ad93b2e39
Gaspard Deltour Bauregard <gaspard.deltour.bauregard@gmail.com>
2022-10-27 17:45:07 +0200

    Changement de base de données

--- subscrition_site/subscrition_site/settings.py
+++ subscrition_site/subscrition_site/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'subscrition_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'users',
+        'USER': 'admin',
+        'PASSWORD': '}}NAMI}}667',
+        'HOST':'localhost',
     }
 }
 
